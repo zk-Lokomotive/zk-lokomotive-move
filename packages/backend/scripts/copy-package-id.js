88ea735bfa03caee2ec8e71b89052c391add6501
wirklichkeit der substanz <asesenep15@gmail.com>
2024-06-18 03:41:30 +0300

    get metadata of files

--- packages/backend/scripts/copy-package-id.js
+++ packages/backend/scripts/copy-package-id.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * The script copies the deployed package ID from the corresponding Suibase network file to .env.local of the frontend package,
- * which is then read by the app.
- * 
- * The default network is localnet. To change it, pass "-n [NETWORK_TYPE]" through console.
- */
-
 const { promises } = require("node:fs");
 const { homedir } = require("node:os");
 const path = require("node:path");
@@ -18,14 +11,8 @@ const main = async () => {
   const network = getNetworkFromArgs();
   const sourceFile = sourceFilePath(network, DEPLOYED_MODULE_NAME);
   const targetFile = targetFilePath();
-
-  // Read package ID from SuiBase packageId file.
   const packageId = await readPackageId(sourceFile);
-
-  // Create .env.local file if it doesn't exist.
   await createFileIfNecessary(targetFile);
-
-  // Add VITE_[network]_CONTRACT_PACKAGE_ID variable to .env.local or update its value if it exists.
   await setEnvVar(
     targetFile,
     `VITE_${network.toUpperCase()}_CONTRACT_PACKAGE_ID`,
@@ -46,47 +33,20 @@ const targetFilePath = () => {
 
 const getNetworkFromArgs = () => {
   const arg = process.argv.slice(2);
-
-  switch (arg[0]) {
-    case "-n":
-      return arg[1];
-
-    default:
-      return "localnet";
-  }
+  return arg[0] === "-n" ? arg[1] : "localnet";
 };
 
-/**
- * Read package ID from SuiBase packageId file.
- *
- * @param {string} sourceFile
- * @returns
- */
 const readPackageId = async (sourceFile) => {
   const data = await promises.readFile(sourceFile, "utf8");
   return JSON.parse(data)[0];
 };
 
-/**
- * Create a file if it doesn't exist.
- *
- * @param {string} filePath
- * @returns
- */
 const createFileIfNecessary = async (filePath) => {
   try {
     await promises.writeFile(filePath, "", { flag: "wx" });
   } catch {}
 };
 
-/**
- * Set the environment variable in the .env.local file.
- *
- * @param {string} envFilePath
- * @param {string} name
- * @param {string} value
- * @returns
- */
 const setEnvVar = async (envFilePath, name, value) => {
   const envFileWriter = new EnvFileWriter(envFilePath, false);
   await envFileWriter.parse();
@@ -94,7 +54,6 @@ const setEnvVar = async (envFilePath, name, value) => {
   await envFileWriter.save();
 };
 
-// Main entry point.
 main().catch((e) => {
   console.error(e);
 });
